d300bcf2793377cbd64b0296b5a0c908612c57c2
xsouffront <43756495+xsouffront@users.noreply.github.com>
2019-01-29 12:40:14 -0500

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,13 @@
+dogDaycare = ['Skye', 'Mabel','Lassie','Buttercup']
+print(dogDaycare)
+dogDaycare.append('Widget')
+dogDaycare.append('Balto')
+dogDaycare.append('Chole')
+print(dogDaycare)
+dogDaycare.sort()
+print(dogDaycare)
+dogDaycare[:3]
+print(dogDaycare)
+dogDaycare.remove('Buttercup')
+dogDaycare.remove('Mabel')
+print(dogDaycare)
